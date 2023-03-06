9950a64b9c61ddb39d7e4582e9ecfd65c38cdf59
vishal-aurasoft <vishalaurasoft@gmail.com>
2023-03-06 14:20:30 +0530

    GYC-mail-comit

--- js/main.js
+++ js/main.js
@@ -9,25 +9,7 @@
             $('.navbar').removeClass('sticky-top');
         }
     });
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
+       
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -42,4 +24,102 @@
     });
     
 })(jQuery);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+function calldata() {
+    var name = document.getElementById("username").value;
+    var contact = document.getElementById("contactno").value;
+    var email = document.getElementById("email").value;
+    var country = document.getElementById("country").value;
+    var course = document.getElementById("course").value;
+    var city = document.getElementById("city").value;
+    
+    Email.send({
+        SecureToken: "321d13fd-4104-4be9-a0ad-1929fd9c577f",
+        To: 'websiteygm@gmail.com',
+        From: "websiteygm@gmail.com",
+        Subject: "mail from contact form",
+        Body: "Name :- " + name + "<br/>Contact :- " + contact + "<br/>Email :- " + email + "<br/>Country :- " + country + "<br/>Course :- " + course + "<br/>City :- " + city
+    }).then(
+        // message => alert(message)
+    );
+
+}
+
+$(function () {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+
+        autoplay: true,
+        rewind: false,
+        loop: true,
+        margin: 20,
+
+        responsiveClass: true,
+        autoHeight: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 800,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1024: {
+                items: 2
+            }
+        }
+    });
+});
+$('.owl-carousel-service').owlCarousel({
+    autoplay: true,
+    rewind: false,
+    loop: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 800,
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        1024: {
+            items: 4
+        }
+    }
+})
+
+$('.owl-carousel-achivers').owlCarousel({
+    autoplay: true,
+    rewind: false,
+    loop: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 800,
+    loop: true,
+    dots: true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        426: {
+            items: 1
+        }
+        ,
+        768: { items: 3 }
+        ,
+        1024: {
+            items: 4
+        }
+    }
+})
+
+AOS.init();
+
+
+
 
